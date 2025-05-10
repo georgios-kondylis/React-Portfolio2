7cf3d2f7c835ac8b0ab9cac7adf9f9e2a79b9221
Georgios <georgios.p.kondylis@gmail.com>
2025-05-10 11:07:40 +0200

    transition1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,28 +22,28 @@ const Navbar = ({ darkMode, setDarkMode, textColor, sideNavIsOpen, setSideNavIsO
         </div>
           : <div></div>}
           {/* NAVLINKS */}
-        <div className={`${textColor} text-[1.2rem] max-lg:text-[1rem] flex items-center gap-4 max-lg:gap-[15px] max-lg:hidden transition-all ease-out duration-300`}> {/* Navlinks */}
-          <a href="#home" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition-all ease-in-out duration-300 `}>Home</a>
-          <a href="#about" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition-all ease-in-out duration-300`}>About</a>
-          <a href="#projects" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition-all ease-in-out duration-300`}>Projects</a>
-          <a href="#testimonials" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition-all ease-in-out duration-300`}>Testimonials</a>
-          <a href="#contact" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition-all ease-in-out duration-300`}>Contact</a>
+        <div className={`${textColor} text-[1.2rem] max-lg:text-[1rem] flex items-center gap-4 max-lg:gap-[15px] max-lg:hidden transition1`}> {/* Navlinks */}
+          <a href="#home" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition1`}>Home</a>
+          <a href="#about" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition1`}>About</a>
+          <a href="#projects" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition1`}>Projects</a>
+          <a href="#testimonials" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition1`}>Testimonials</a>
+          <a href="#contact" className={`${!darkMode? 'hover:text-[#4a4a4a]' : 'hover:text-[#f5f3dc]'}  hover:scale-110 transition1`}>Contact</a>
         </div>
           {/* Dark & Brightmode Toggle BTN */}
         <div className="flex items-center gap-[20px]">
             <SwichBtn darkMode={darkMode} setDarkMode={setDarkMode}/>
             <div className={`${textColor}  flex flex-col gap-[6px] lg:hidden cursor-pointer`} 
                   onClick={toggleSideNav}>
-                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full  ${sideNavIsOpen ? 'rotate-45 translate-y-[8px]' : ''} transition-all ease-in-out duration-300`}></span>
-                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full ${sideNavIsOpen ? 'opacity-0' : ''} transition-all ease-in-out duration-300`}></span>
-                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full ${sideNavIsOpen ? '-rotate-45 translate-y-[-7px]' : ''} transition-all ease-in-out duration-300`}></span>
+                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full  ${sideNavIsOpen ? 'rotate-45 translate-y-[8px]' : ''} transition1`}></span>
+                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full ${sideNavIsOpen ? 'opacity-0' : ''} transition1`}></span>
+                <span className={`${!darkMode? 'bg-black' : 'bg-white'} w-[25px] h-[2px] rounded-full ${sideNavIsOpen ? '-rotate-45 translate-y-[-8px]' : ''} transition1`}></span>
             </div>
         </div>
       
         {sideNavIsOpen && <div onClick={() => setSideNavIsOpen(false)} className={`fixed top-[70px] h-[100vh] left-0 bottom-0 right-0 bg-[#04040498] z-0`}></div> }
 
         {/* -------  sideNav  ------- */}
-        <div className={`px-[5px] fixed top-0 left-0 ${!sideNavIsOpen ? 'right-[100%] opacity-0 pointer-events-none' :  'right-[60%] max-w-[250px]' } bottom-0 ${darkMode? 'bg-[#121212]' : 'bg-[#eee4d9]'}  z-50 transition-all ease-in-out duration-300 border-r border-r-[#5c5c5c49]
+        <div className={`px-[5px] fixed top-0 left-0 ${!sideNavIsOpen ? 'right-[100%] opacity-0 pointer-events-none' :  'right-[60%] max-w-[250px]' } bottom-0 ${darkMode? 'bg-[#121212]' : 'bg-[#eee4d9]'}  z-50 transition1 border-r border-r-[#5c5c5c49]
             flex flex-col items-start`}>
     
           <div className={`border-b pl-[10px] w-full flex items-center justify-start h-[70px] ${darkMode? 'border-b-[rgba(225,225,225,0.07)]' : 'border-b-[rgba(16,16,16,0.25)]'} `}>
